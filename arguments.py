00bdc85f806eea83168983de131b44359fc74fff
Charles Edward Gagnon <Charles-Edward.Gagnon@drdc-rddc.gc.ca>
2025-05-12 13:35:26 -0400

    pipeline refactor

---
+++ arguments.py
@@ -0,0 +1,45 @@
+import argparse
+
+BASE_PROMPT = """### Assembly Code ###
+```assembly
+{assembly}
+```
+
+### Instructions ###
+{instructions}
+
+### Output Format ###
+The output must match exactly the following `JSON` schema:
+
+{format}
+
+### Output ###
+"""
+
+class Arguments:
+    model: str # Name of the model to use. First check our model dictionary and default to using the name as a HuggingFace model
+    prompt: str # File or string containing the prompt to use
+    assembly: str # File containing the assembly to analyze
+
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description="Pipeline tool for assembly feature extraction"
+        )
+        parser.add_argument("-m", "--model", choices=["codeqwen"], type=str, default="codeqwen")
+        parser.add_argument("-p", "--prompt", type=str, default="base")
+        parser.add_argument("assembly", metavar="ASM-FILE", type=str)
+
+        arguments = parser.parse_args(namespace=Arguments)
+        self = arguments
+    
+    def format_prompt(self) -> str:
+        with open(f"prompts/{self.prompt}/prompt.txt", "r") as prompt_file:
+            prompt = prompt_file.read()
+        
+        with open(f"prompts/{self.prompt}/format.jsonc", "r") as format_file:
+            format = format_file.read()
+
+        with open(self.assembly, "r") as assembly_file:
+            assembly = assembly_file.read()
+
+        return BASE_PROMPT.format(prompt=prompt, format=format, assembly=assembly)
--- main.py
+++ main.py
@@ -1,51 +1,24 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import argparse
+from arguments import Arguments
 
-PROMPT = """
-Here is a function in assembly:
-
-```assembly
-{assembly}
-```
-
-Here are its main features:
-"""
+MODELS = {
+    "codeqwen": "Qwen/Qwen2.5-Coder-0.5B-Instruct"
+}
 
 def main():
-    args = parse_arguments()
-    print(args.model)
+    arguments = Arguments()
 
-    # model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-Coder-0.5B-Instruct", torch_dtype="auto", device_map="auto").to("cuda")
-    # tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-0.5B-Instruct")
+    model = AutoModelForCausalLM.from_pretrained(MODELS[arguments.model], torch_dtype="auto", device_map="auto").to("cuda")
+    tokenizer = AutoTokenizer.from_pretrained(MODELS[arguments.model])
 
-    # content = None
-    # with open("simple.s", "r") as file:
-    #     content = file.read()
+    query = arguments.format_prompt()
 
-    # query = PROMPT.format(assembly=content)
+    model_inputs = tokenizer([query], return_tensors="pt").to("cuda")
 
-    # model_inputs = tokenizer([query], return_tensors="pt").to("cuda")
+    tokenized_output = model.generate(**model_inputs, max_new_tokens=200, temperature=0.5)
+    output = tokenizer.batch_decode(tokenized_output)[0]
 
-    # tokenized_output = model.generate(**model_inputs, max_new_tokens=200, temperature=0.5)
-    # output = tokenizer.batch_decode(tokenized_output)[0]
-    # print(output)
-
-class Arguments:
-    model: str # Name of the model to use. First check our model dictionary and default to using the name as a HuggingFace model
-    prompt: str # File or string containing the prompt to use
-    format: str # File or string containing the format to use
-    assembly: str # File containing the assembly to analyze
-
-def parse_arguments() -> Arguments:
-    parser = argparse.ArgumentParser(
-        description="Pipeline tool for assembly feature extraction"
-    )
-    parser.add_argument("-m", "--model", choices=["codeqwen"], type=str, default="codeqwen")
-    parser.add_argument("-p", "--prompt", choices=["base"], type=str, default="base")
-    parser.add_argument("-f", "--format", choices=["base"], type=str, default="base")
-    parser.add_argument("assembly", metavar="ASM-FILE", type=str)
-
-    return parser.parse_args(namespace=Arguments)
+    print(output)
 
 if __name__ == "__main__":
     main()
