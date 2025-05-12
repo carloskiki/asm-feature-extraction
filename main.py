647994c1b989ac3a7ddb76fdfd753b03f14b2ad4
Charles Edward Gagnon <Charles-Edward.Gagnon@drdc-rddc.gc.ca>
2025-05-12 07:05:10 -0400

    brought changes from server

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# PROMPT = """
+# Determine the genera class (examples: Cryptographic, C Runtime, Networking) of the function that this assembly routine belongs to.
+# 
+# ```assembly
+# {assembly}
+# ```
+# 
+# Function Class:
+# """
+
 PROMPT = """
-Determine the genera class (examples: Cryptographic, C Runtime, Networking) of the function that this assembly routine belongs to.
+Here is a function in assembly. We explain its main features.
 
-```assembly
-{assembly}
 ```
-
-Function Class:
 """
+
+def main():
+    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-Coder-0.5B-Instruct", torch_dtype="auto", device_map="auto").to("cuda")
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-0.5B-Instruct")
+
+    content = None
+    with open("simple.s", "r") as file:
+        content = file.read()
+
+    query = PROMPT + content + "\n```"
+
+    model_inputs = tokenizer([query], return_tensors="pt").to("cuda")
+
+    tokenized_output = model.generate(**model_inputs, max_length=500)
+    output = tokenizer.batch_decode(tokenized_output)[0]
+    print(output)
+
+if __name__ == "__main__":
+    main()
