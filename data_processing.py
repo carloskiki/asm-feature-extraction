e2de4d1596f07372fdebddd52c10578fffbc1aee
Charles Edward Gagnon <Charles-Edward.Gagnon@drdc-rddc.gc.ca>
2025-05-12 15:05:51 -0400

    Data processing module

---
+++ data_processing.py
@@ -0,0 +1,69 @@
+from typing import Generator
+import json
+
+class Instruction:
+    address: int
+    mnemonic: str
+    operands: list[str]
+
+    def __init__(self, address: int, mnemonic: str, operands: list[str]):
+        self.address = address
+        self.mnemonic = mnemonic
+        self.operands = operands
+
+    def __str__(self):
+        return f"    0x{self.address:X} {self.mnemonic} " + ' '.join(self.operands)
+
+class Block:
+    label: str
+    instructions: list[Instruction]
+
+    def __init__(self, label: str, instructions: list[Instruction]):
+        self.label = label
+        self.instructions = instructions
+    
+    def __str__(self):
+        return f"{self.label}:\n" + '\n'.join(str(i) for i in self.instructions)
+
+class Function:
+    name: str
+    start: int
+    end: int
+    blocks: list[Block]
+
+    def __init__(self, name: str, start: int, end: int, blocks: list[Block]):
+        self.name = name
+        self.start = start
+        self.end = end
+        self.blocks = blocks
+
+    def __str__(self):
+        return f"{self.name}:\n" + '\n'.join(str(b) for b in self.blocks)
+
+def process(file: str) -> Generator[Function, None, None]:
+    with open(file, "r") as file:
+        data = json.loads(file.read())
+
+    index: int = 0
+    for function in data["functions"]:
+        name = function["name"]
+        start = function["addr_start"]
+        end = function["addr_end"]
+        blocks = []
+
+        for _ in range(function["bbs_len"]):
+            block = data["blocks"][index]
+            label = block["name"]
+            instructions = []
+
+            for instruction in block["ins"]:
+                address = instruction["ea"]
+                mnemonic = instruction["mne"]
+                operands = instruction["oprs"]
+                instructions.append(Instruction(address, mnemonic, operands))
+
+            blocks.append(Block(label, instructions))
+
+            index += 1
+        
+        yield Function(name, start, end, blocks)
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+# from transformers import AutoModelForCausalLM, AutoTokenizer
 from arguments import Arguments
+from data_processing import process
 
 MODELS = {
     "codeqwen": "Qwen/Qwen2.5-Coder-0.5B-Instruct"
 }
 
 def main():
-    arguments = Arguments()
+    # arguments = Arguments()
 
-    model = AutoModelForCausalLM.from_pretrained(MODELS[arguments.model], torch_dtype="auto", device_map="auto").to("cuda")
-    tokenizer = AutoTokenizer.from_pretrained(MODELS[arguments.model])
+    functions = process("C:/Users/cgagnon/ghidra-plugin/feature-extractor/data/openssl-gcc32-g-O0.bin.merged.asm.json")
+    print(next(x for i,x in enumerate(functions) if i==150))
 
-    query = arguments.format_prompt()
+    # model = AutoModelForCausalLM.from_pretrained(MODELS[arguments.model], torch_dtype="auto", device_map="auto").to("cuda")
+    # tokenizer = AutoTokenizer.from_pretrained(MODELS[arguments.model])
 
-    model_inputs = tokenizer([query], return_tensors="pt").to("cuda")
+    # query = arguments.format_prompt()
 
-    tokenized_output = model.generate(**model_inputs, max_new_tokens=2000, temperature=0.5)
-    output = tokenizer.batch_decode(tokenized_output)[0]
+    # model_inputs = tokenizer([query], return_tensors="pt").to("cuda")
 
-    print(output)
+    # tokenized_output = model.generate(**model_inputs, max_new_tokens=2000, temperature=0.5)
+    # output = tokenizer.batch_decode(tokenized_output)[0]
+
+    # print(output)
 
 if __name__ == "__main__":
     main()
