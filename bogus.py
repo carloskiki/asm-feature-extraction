9e44d1ed8e74e443b050b186fdf92275231c4d28
Charles Edward Gagnon <Charles-Edward.Gagnon@drdc-rddc.gc.ca>
2025-06-13 10:40:03 -0400

    check that larger pools also mostly work

--- bogus.py
+++ bogus.py
@@ -26,8 +26,7 @@ class Bogus(context.Context):
 
     
     def __call__(self):
-        dataset = LibDataset(self.data_path, main_process=True, pool_size=16, binary="openssl", optimization=2, seed=40332319787)
+        dataset = LibDataset(self.data_path, main_process=True, pool_size=2048, binary="openssl", optimization=2, seed=40332319787)
         pool_dataset = TargetDataset(dataset, optimization_diff=1, platform_diff=None)
         
-        print([f.name for f in dataset.flattened])
-        print([f.name for f in pool_dataset.flattened])
+        assert([f.name for f in dataset.flattened] == [f.name for f in pool_dataset.flattened])
