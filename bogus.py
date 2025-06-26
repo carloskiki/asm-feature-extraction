13101b2173dfa6044f101710b16a1622a6c24fa7
Charles Edward Gagnon <Charles-Edward.Gagnon@drdc-rddc.gc.ca>
2025-06-26 09:22:00 -0400

    bogus extract functions from dataset

--- bogus.py
+++ bogus.py
@@ -5,9 +5,11 @@ Doesn't do much, just to play around & try stuff
 from dataclasses import dataclass
 from argparse import ArgumentParser
 import context
+from data_processing import LibDataset
 
 @dataclass
 class Bogus(context.Context):
+    out_file: str
 
     @staticmethod
     def arguments(subparsers):
@@ -19,6 +21,14 @@ class Bogus(context.Context):
             "bogus",
             description="",
         )
+        parser.add_argument("out_file", type=str)
 
     def __call__(self):
-        pass
+        dataset = LibDataset("lib-data", True, 1000, None, "openssl", 2, "gcc")
+
+        with open(self.out_file, "w", encoding="utf-8") as file:
+            for (func, _) in dataset:
+                file.write("#####\n")
+                file.write(func.name + "\n")
+                file.write(func)
+                file.write("\n\n")
