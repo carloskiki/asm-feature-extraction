15308f82975ab47fcf697d11a194b2086eb33ae2
Charles Edward Gagnon <Charles-Edward.Gagnon@drdc-rddc.gc.ca>
2025-06-30 13:57:36 -0400

    unsqueeze

--- retrieval.py
+++ retrieval.py
@@ -256,7 +256,7 @@ class Retrieval(Context):
         wrong_json_indices = [i for i, x in enumerate(generated) if x is None]
 
         for index in wrong_json_indices:
-            indexed_tokens = { key: val[index] for key, val in tokens.items() }
+            indexed_tokens = { key: val[index].unsqueeze(0) for key, val in tokens.items() }
 
             for x in range(3):
                 print(f"got invalid JSON, retrying {x + 1}/3")
